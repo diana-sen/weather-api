74258d91704b9ad7e6dfd30b88e1e8ca064c1b89
Diana Sen <dianasen13@gmail.com>
2022-07-28 19:02:25 -0500

    feat: add featchWeather function

--- index.js
+++ index.js
@@ -5,12 +5,27 @@ let CLIENT = process.env.aKey;
 
 let weather = {
 apikey: CLIENT,
-fetchWeather: async function() {
+fetchWeather: async function(city) {
     await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=${CLIENT}`
-    ).then((response) => response.json())
-    .then((data) => console.log(data));
+        `https://api.openweathermap.org/data/2.5/weather?q=`
+        + city+ "&units=metric&appid=" + this.apikey)
+        .then((response) => response.json())
+        //.then((data) => console.log(data));
+        .then((data)=> this.showWeather(data));
 },
+showWeather: function(data) {
+    const {name} = data;
+    const {icon, description} = data.weather[0];
+    const {temp, humidity} = data.main;
+    const {speed} = data.wind;
+    //console.log(name,icon,description,temp,humidity,speed);
+    
+   // document.getElementById(".city").innerHTML= "Weather in" + name;
+   
+}
+
 };
 
-weather.fetchWeather();
+
+weather.fetchWeather("Tokyo");
+
